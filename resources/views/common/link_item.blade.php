25946718579001f4a9c3707adcb8337cdf8b893a
daazuma <d_azuma@datastadium.co.jp>
2023-07-19 10:34:21 +0900

    create top page

---
+++ resources/views/common/link_item.blade.php
@@ -0,0 +1,15 @@
+@if (Auth::check())
+    {{-- linktoviewalluser --}}
+    <li><a class="link link-hover" href="#">User</a></li>
+    {{-- link to user details --}}
+    <li><a href="#" class="link link-hover">{{ Auth::user()->name }}&#39;s plofile</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- link to logout --}}
+    <li><a href="#" class="link link-hover" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
+@else
+    {{-- link to register user --}}
+    <li><a class="link link-hover" href="{{ route('register') }}">Signup</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- link to login page --}}
+    <li><a href="{{ route('login') }}" class="link ">Login</a></li>
+@endif
